3a44f3dc5005734b5ea864bc562fcd036070c35a
Prateek Angel <prateekangel8127@gmail.com>
2023-11-10 04:29:06 -0500

    adding all files

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<div class="the-title">
-<h1><?php the_title(); ?></h1>
-</div>
-<div class="content-image">
-    <?php the_content(); ?>
-</div>
-<?php get_footer(); ?>
-
--- single.php
+++ single.php
@@ -1,10 +1,18 @@
-<?php get_header(); ?>
-<?php
-/*start the loop */
+<div class="single/php">
+    <?php get_header(); ?>
+    <?php
 while(have_posts()):
     the_post();
     the_title();
+    the_post_thumbnail();
+    the_content();
+    the_tags();
+    the_category();
+    the_author();
+    the_author_meta('description');
+    the_date();
+    the_time();
 
 endwhile;
 ?>
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
